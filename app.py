ad5b4d3d1f7356ff28ceb9ad3e4fa36c861ef105
Your Name <you@example.com>
2024-07-05 19:03:36 +0900

    first commit

---
+++ app.py
@@ -0,0 +1,10 @@
+def kuku(max_dan):
+    for i in range(1, max_dan + 1):
+        for j in range(1, 10):
+            print(f"{i}X{j}={i*j}", end="\t")
+
+        print()
+
+
+max_dan = int(input("何段までの九九を表示しますか："))
+kuku(max_dan)
